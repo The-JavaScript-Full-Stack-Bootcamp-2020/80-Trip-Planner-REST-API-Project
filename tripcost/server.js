b334184bf4c067aa39ce0dc42dfff73d12f8b11b
Hashy <59665246+copev313@users.noreply.github.com>
2020-08-09 15:43:42 -0400

    POST /trip

--- tripcost/server.js
+++ tripcost/server.js
@@ -2,18 +2,47 @@ const express = require('express');
 const mongo = require('mongodb').MongoClient;
 
 const app = express();
+const url = 'mongodb://localhost:27017';
 
+app.use(express.json());
+
+let db, trips, expenses;
+
+mongo.connect(url, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    }, (err, client) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        db = client.db('tripcost');
+        trips = db.collection('trips');
+        expenses = db.collection('expenses');
+    }
+);
 ////////////////////////////////////
 
-app.post('/trip', (req, res) => { /* */ });
+app.post('/trip', (req, res) => { 
+    const name = req.body.name
+    trips.insertOne({ name: name }, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ err: err });
+            return;
+        }
+        console.log(result);
+        res.status(200).json({ ok: true });
+    });
+
+});
 
 app.get('/trips', (req, res) => { /* */ });
 
 app.post('/expense', (req, res) => { /* */ });
 
 app.get('/expenses', (req, res) => { /* */ });
-
-
 
 
 ///////////////////////////////////
